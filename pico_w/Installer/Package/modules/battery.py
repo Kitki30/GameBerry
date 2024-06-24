02d0d310dc1493baf301369d72a00593f3414c58
Kitki30 <84147906+Kitki30@users.noreply.github.com>
2024-06-24 14:26:40 +0200

    Delete pico_w/Installer directory

--- pico_w/Installer/Package/modules/battery.py
+++
@@ -1,7 +0,0 @@
-import machine
-
-vbus_pin = machine.Pin(24, machine.Pin.IN)
-
-def is_usb_powered():
-    return vbus_pin.value()
-
--- pico_w/Installer/linux.sh
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt --break-system-packages
-python main.py
