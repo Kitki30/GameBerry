9b8edd4437ee8a68f888ced0ebf50683b3285598
Kitki30 <krystianrk12@gmail.com>
2024-06-24 14:38:14 +0200

    xd

--- pico_w/Gameberry/modules/translations.py
+++ pico_w/Gameberry/modules/translations.py
@@ -2,8 +2,11 @@ import modules.json as json
 import modules.files as files
 import modules.customExceptions as exceptions
 import network
+import modules.requests as requests
+import modules.files as files
 
 translation = None
+updatePath = "https://raw.githubusercontent.com/Kitki30/GameBerry/main/pico_w/Gameberry/translations/"
 
 def load(lang = "en"):
     global translation
@@ -25,7 +28,18 @@ def get(thing):
 def update(lang = "en"):
     wlan = network.WLAN(network.STA_IF)
     if wlan.isconnected() == True:
-        # Do update logic
-        return True
+        request = requests.GET(updatePath+lang+".json")
+        if request.status_code == 200:
+            if files.exist("/translations/"+lang+".json"):
+                translation_json = json.read("/translations/"+lang+".json")
+                request_json = request.json()
+                if translation_json["info"]["version"] < request_json["info"]["version"]:
+                    print("Updating translation...")
+                else:
+                    return True
+            else:
+                with open('/translations/'+lang+".json", 'wb') as file:
+                    file.write(request.content)
+
     else:
         raise exceptions.WifiNotConnected("Wi-Fi is not connected, cannot update Translations!")
